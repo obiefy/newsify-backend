ca26733d967e93483a7c808c7a9e1df22916645a
Obay Adam <obayhamed@gmail.com>
2023-03-11 01:19:10 +0300

    feature: retrive news from platforms

---
+++ app/Http/Controllers/NewsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\News\Collector;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+
+class NewsController extends Controller
+{
+    public function feed() {
+        try {
+            $news = Collector::feed();
+            return $this->ok($news);
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+
+    public function search(Request $request) {
+        try {
+            $filters = array_filter($request->only(['keyword', 'date', 'category', 'source']));
+
+            $news = Collector::filter($filters);
+
+            return $this->ok($news);
+        } catch (\Exception $e) {
+            return $this->error($e->getMessage());
+        }
+    }
+}
