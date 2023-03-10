eacbf90c4f6bbb9d7861107ed63c2d4e98bce845
Obay Adam <obayhamed@gmail.com>
2023-03-11 01:16:36 +0300

    feature: prepare interfaces for news platforms

---
+++ app/News/NewsInterface.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\News;
+
+interface NewsInterface {
+    
+    public static function make(): static;
+
+    public function url(): string;
+
+    public function params(): array;
+
+    public function withParams(string $key, string $value): static;
+
+    public function filter(array $filters): static;
+
+    public function search(string $keyword): static;
+
+    public function get(): array;
+
+    public function format(array $articles): array;
+
+    public function publicationDate(string $date): string;
+    
+}
