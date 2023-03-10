9fc115a77767b4ed8c8008ffc85a5d47935cf7e0
Obay Adam <obayhamed@gmail.com>
2023-03-11 01:12:49 +0300

    feature: add controller responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -9,4 +9,23 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function ok(array $data): array {
+        return $this->response(200, 'Success', $data);
+    }
+
+    public function error(string $message = null, int $status = 500): array {
+        $message = $message ?? 'Server Error, please try again later.';
+
+        return $this->response($status, $message);
+    }
+
+    public function response(int $status, string $message, array $data = []): array {
+        return [
+            'status' => $status,
+            'message' => $message,
+            'data' => $data,
+        ];
+    }
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return ['Laravel' => app()->version()];
-});
+Route::get('/', [NewsController::class, 'feed']);
+Route::get('/search', [NewsController::class, 'search']);
 
 require __DIR__.'/auth.php';
